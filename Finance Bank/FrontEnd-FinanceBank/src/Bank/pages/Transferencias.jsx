eb7b92f347e459dc9c00358fb7874edddd456661
Estuardo <jrodas-2021418@kinal.edu.gt>
2023-06-29 14:50:37 -0600

    Se agrego estuructura de transferencias

--- Finance Bank/FrontEnd-FinanceBank/src/Bank/pages/Transferencias.jsx
+++ Finance Bank/FrontEnd-FinanceBank/src/Bank/pages/Transferencias.jsx
@@ -1,6 +1,88 @@
 
 export const Transferencias = () => {
   return (
-    <h1>Transferencias</h1>
+    <>
+
+    <form>
+
+      <div className="container">
+        <div className="row justify-content-center ">
+          <div className="col-md-10">
+  <div className="form-group row">
+
+    <label for="inputEmail3" class="col-sm-2 col-form-label">Tipo de Transferencia:</label>
+
+    <div className="col-sm-10">
+
+      <input type="email" class="form-control" id="inputEmail3" placeholder="Ejem. Débitos"/>
+
+    </div>
+
+  </div>
+
+  <div className="form-group row">
+
+    <label for="inputPassword3" class="col-sm-2 col-form-label">Numero de cuenta:</label>
+
+    <div className="col-sm-10">
+
+      <input type="password" class="form-control" id="inputPassword3" placeholder="Ejem. 1234567890"/>
+
+    </div>
+
+  </div>
+
+  <div className="form-group row">
+
+    <label for="inputPassword3" class="col-sm-2 col-form-label">Destinatario:</label>
+
+    <div className="col-sm-10">
+
+      <input type="password" class="form-control" id="inputPassword3" placeholder="Ejem. Persona1"/>
+
+    </div>
+
+  </div>
+
+  <div className="form-group row">
+
+    <label for="inputPassword3" class="col-sm-2 col-form-label">Monto:</label>
+
+    <div className="col-sm-10">
+
+      <input type="password" class="form-control" id="inputPassword3" placeholder="Q.00"/>
+
+    </div>
+
+  </div>
+
+  <div className="form-group row">
+
+    <label for="inputPassword3" class="col-sm-2 col-form-label">Fecha Emitida:</label>
+
+    <div className="col-sm-10">
+
+      <input type="password" class="form-control" id="inputPassword3" placeholder="YY/MM/DD"/>
+
+    </div>
+
+  </div>
+
+  <div className="form-group row">
+
+    <div className="col-sm-10">
+
+      <button type="submit" class="btn btn-primary">Transferir</button>
+
+    </div>
+    </div>
+  </div>
+  </div>
+
+  </div>
+
+</form>
+
+    </>
   )
 }
