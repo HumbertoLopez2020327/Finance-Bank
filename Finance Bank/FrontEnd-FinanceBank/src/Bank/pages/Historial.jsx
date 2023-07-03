3c26fcee3168c72a570dafb5fcb16e308c7c89b7
Cristian Suar <csuar-2017085@kinal.edu.gt>
2023-07-03 11:43:08 -0600

    Se agrego estructura al hitorial

--- Finance Bank/FrontEnd-FinanceBank/src/Bank/pages/Historial.jsx
+++ Finance Bank/FrontEnd-FinanceBank/src/Bank/pages/Historial.jsx
@@ -2,6 +2,18 @@
 
 export const Historial = () => {
   return (
-    <h1>Historial</h1>
+    <div className="container">
+      <div className="row justify-content-center">
+        <div className="col-md-6">
+          <h1>Historial</h1>
+          <ul className="list-group">
+            <li className="list-group-item">registro</li>
+            <li className="list-group-item">registro</li>
+            <li className="list-group-item">registro</li>
+            <li className="list-group-item">registro</li>
+          </ul>
+        </div>
+      </div>
+    </div>
   )
 }
